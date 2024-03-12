14d8755e488941e14f2f19332df59c8d10e79b5e
Shivesh Kumar <87690329+shivesh41kr@users.noreply.github.com>
2024-03-12 20:39:16 +0530

    Delete Online-Code-Runner-master directory

--- Online-Code-Runner-master/backend/codes/69d7aa5e-fea5-43b6-b898-6bb09aedab95.cpp
+++
@@ -1,8 +0,0 @@
-// Xtend Algo : Code Editor - C++
-#include <iostream>
-using namespace std;
-
-int main() {
-    cout << "Hello World";
-    return 0;
-}
--- Online-Code-Runner-master/backend/codes/db5d7abf-78ee-4cbd-af6a-11b653725c54.py
+++
@@ -1,7 +0,0 @@
-# python3
-
-import sys
-sys.stdin  = open(sys.argv[1])
-# keep the above lines to take user input
-
-print("Hello World !!!")
